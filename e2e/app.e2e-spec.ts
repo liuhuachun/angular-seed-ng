76b661d71ed73ebdbfef4ded33212c3d3e71b528
Angular CLI <angular-cli@angular.io>
2017-04-28 17:35:24 +0800

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AngularSeedNgPage } from './app.po';
+
+describe('angular-seed-ng App', () => {
+  let page: AngularSeedNgPage;
+
+  beforeEach(() => {
+    page = new AngularSeedNgPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
