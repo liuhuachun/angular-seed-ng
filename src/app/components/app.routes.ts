e128d8ed0d388cfed8dc72a77660c7567eb91d40
liu carl <liu carl>
2017-04-30 16:35:56 +0800

    增加异步加载的例子

--- src/app/components/app.routes.ts
+++ src/app/components/app.routes.ts
@@ -1,3 +1,5 @@
 // app
 
-export const routes: Array<any> = [];
+export const routes: Array<any> = [
+  { path: 'about', loadChildren: 'app/components/system/about/about.module#AboutModule' }
+];
---
+++ src/app/components/system/about/about.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { AboutRoutingModule } from './about.routes';
+import { AboutComponent } from './about.component';
+
+@NgModule({
+  imports : [ AboutRoutingModule ],
+  declarations : [ AboutComponent ]
+})
+export class AboutModule { }
